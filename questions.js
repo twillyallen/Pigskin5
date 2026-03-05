39afd4d0cf342404bad39a66e811d7e21291e485
twillyallen <twillyallen@gmail.com>
2026-03-04 16:17:45 -0800

    New Questions

--- questions.js
+++ questions.js
@@ -41,6 +41,76 @@ export const CALENDAR = {
     { question: "Which WR performed a DISGUSTING act and pretended to moon the crowd in Green Bay?", choices: ["Randy Moss", "Terrell Owens", "Antonio Brown", "Chad Johnson"], answer: 0 },
     ],
   },
+    "2026-03-05": { //Thursday
+    event: "",
+    questions: [
+    { question: "Who has the most Receiving Yards for an Atlanta Falcons TE ever?", choices: ["Kyle Pitts", "Tony Gonzalez", "Jim Mitchell", "Arthur Cox"], answer: 2 },
+    { question: "OVER or UNDER:\nThe Vikings have O/U 3.5 Super Bowl Losses", choices: ["OVER 3.5 Super Bowl Losses", "UNDER 3.5 Super Bowl Losses"], answer: 0 },
+    { question: "Which team has appeared in the most Super Bowls?", choices: ["Green Bay Packers", "Dallas Cowboys", "New England Patriots", "Pittsburgh Steelers"], answer: 2 },
+    { question: "What was the most amount of points scored by one team in a Super Bowl?", choices: ["41", "49", "55", "58"], answer: 2 },
+    { question: "Which team originally drafted Bo Jackson?", choices: ["Browns", "Buccaneers", "Raiders", "Chiefs"], answer: 1 },
+    ],
+  },
+    "2026-03-06": { //Friday
+    event: "",
+    questions: [
+    { question: "Which of these players has more career Receiving Yards?", choices: ["Davante Adams", "Antonio Brown", "Calvin Johnson", "Hines Ward"], answer: 0 },
+    { question: "Which team did the Ravens defeat in Super Bowl XXXV?", choices: ["Raiders", "Giants", "49ers", "Vikings"], answer: 1 },
+    { question: "Which team drafted Drew Brees?", choices: ["Dolphins", "Saints", "Chargers", "Oilers"], answer: 2 },
+    { question: "Who led the 2017 Season in Passing Yards?", choices: ["Tom Brady", "Carson Wentz", "Ben Roethlisberger", "Russell Wilson"], answer: 3 },
+    { question: "What year did the AFL and NFL officially merge?", choices: ["1966", "1970", "1975", "1980"], answer: 1 },
+    ],
+  },
+    "2026-03-07": { //Saturday
+    event: "",
+    questions: [
+    { question: "Which team was once known as the 'Dallas Texans'?", choices: ["Tennessee Titans", "Kansas City Chiefs", "Houston Texans", "Cleveland Browns"], answer: 1 },
+    { question: "Which quarterback was nicknamed “Captain Comeback”?", choices: ["Roger Staubach", "Joe Montana", "Tom Brady", "Fran Tarkenton"], answer: 0 },
+    { question: "Guess the RB Career:\n- 12,979 Yards from Scrimmage\n- 98 Total TDs\n- 4.6 Yards per Carry", choices: ["Alvin Kamara", "Aaron Jones", "Christian McCaffrey", "Marshawn Lynch"], answer: 2 },
+    { question: "Who did Barry Sanders share his MVP Award with?", choices: ["Emmitt Smith", "Troy Aikman", "Terrell Davis", "Brett Favre"], answer: 3 },
+    { question: "Who led the 2025 season in Rushing TDs?", choices: ["Josh Allen", "James Cook", "Jonathan Taylor", "Derrick Henry"], answer: 2 },
+    ],
+  },
+    "2026-03-08": { //Sunday
+    event: "",
+    questions: [
+    { question: "Which franchise has the most players in the Pro Football Hall of Fame?", choices: ["Chicago Bears", "Dallas Cowboys", "Washington Redskins/Commanders", "Green Bay Packers"], answer: 0 },
+    { question: "Which QB has more CAREER Passing TDs?", choices: ["Terry Bradshaw", "Ryan Fitzpatrick", "Josh Allen", "Matt Hasselbeck"], answer: 1 },
+    { question: "Who is the youngest QB to win a Super Bowl?", choices: ["Patrick Mahomes", "Dan Marino", "Ben Roethlisberger", "Jalen Hurts"], answer: 2 },
+    { question: "OVER or UNDER: The Lions have appeared in O/U 0.5 Super Bowls", choices: ["OVER 0.5 Super Bowls", "UNDER 0.5 Super Bowls"], answer: 1 },
+    { question: "Which QB led the biggest comeback in NFL history (33 points)?", choices: ["Matt Ryan", "Kirk Cousins", "Tom Brady", "Josh Allen"], answer: 1 },
+    ],
+  },
+    "2026-03-09": { //Monday
+    event: "",
+    questions: [
+    { question: "Who has the most Yards from Scrimmage in a single season?", choices: ["Chris Johnson", "Barry Sanders", "Marshall Faulk", "Derrick Henry"], answer: 0 },
+    { question: "True or False: The Tampa Bay Buccaneers once lost 26 straight games.", choices: ["True", "False"], answer: 0 },
+    { question: "Which team did the Chiefs defeat in Super Bowl IV?", choices: ["Colts", "Packers", "Vikings", "Jets"], answer: 2 },
+    { question: "Which NFL Team ACTUALLY Existed?", choices: ["Moline Tractors", "Long Island Wave Breakers", "Cleveland Pros", "Dayton Triangles"], answer: 3 },
+    { question: "Which WR caught 23 touchdowns in the 2007 season?", choices: ["Randy Moss", "Terrell Owens", "Marvin Harrison", "Calvin Johnson"], answer: 0 },
+    ],
+  },
+    "2026-03-10": { //Tuesday
+    event: "",
+    questions: [
+    { question: "Which Super Bowl was the first to go into overtime?", choices: ["Bills vs Giants", "49ers vs Ravens", "Colts vs Cowboys", "Patriots vs Falcons"], answer: 3 },
+    { question: "Which player scored the “Beast Quake” touchdown run in the 2010 playoffs?", choices: ["Shaun Alexander", "Reggie Bush", "Marshawn Lynch", "Adrian Peterson"], answer: 2 },
+    { question: "What is the most amount of TDs Davante Adams has had in a single season?", choices: ["14", "18", "20", "21"], answer: 1 },
+    { question: "What Bengal was famous for his post-TD dance, 'The Ickey Shuffle'?", choices: ["James Brooks", "Elbert Woods", "Cris Collinsworth", "Boomer Esiason"], answer: 1 },
+    { question: "Which running back rushed for 296 yards in a single game (NFL record)?", choices: ["Adrian Peterson", "Jamal Lewis", "Reggie Bush", "Derrick Henry"], answer: 0 },
+    ],
+  },
+    "2026-03-11": { //Wednesday
+    event: "",
+    questions: [
+    { question: "Which RB from the 2017 Draft Class has the most career Rushing Yards?", choices: ["Aaron Jones", "Christian McCaffrey", "Joe Mixon", "Alvin Kamara"], answer: 0 },
+    { question: "Who won Offensive Rookie of the Year in 2025?", choices: ["Emeka Egbuka", "Tyler Shough", "Tetairoa McMillan", "Jaxson Dart"], answer: 2 },
+    { question: "Which sports franchise originally drafted Russell Wilson?", choices: ["Saints", "Seahawks", "Packers", "Colorado Rockies"], answer: 3 },
+    { question: "Who was the first player with 4 Super Bowl rings?", choices: ["Terry Bradshaw", "Marv Fleming", "Tom Brady", "Adam Vinatieri"], answer: 1 },
+    { question: "Which WR has more Career Receiving Yards?", choices: ["Doug Baldwin", "Cris Collinsworth", "Michael Thomas", "Ricky Sanders"], answer: 1 },
+    ],
+  },
 };
 
 
